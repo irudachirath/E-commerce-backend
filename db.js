022fb7d9a748b7b27e0f734edade634d8114ce7a
Iruda Chirath <128205310+irudachirath@users.noreply.github.com>
2024-08-16 22:32:24 +0530

    Update db.js for testing

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const pool = require("mysql2/promise");
 const mysqlPool = pool.createPool({
   host: "ecommerce-db.ctgs2e2oeqo4.ap-southeast-1.rds.amazonaws.com",
   user: "admin",
-  password: process.env.DB_PASSWORD,
+  password: "MySQL_Group32",
   database: "group32_V1.0",
-  port: process.env.DB_PORT,
+  port: 3305,
 });
 
 module.exports = mysqlPool;
